b3a7eb17656ec3fa10b3d8b6aaa5d728b9872daf
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-11 17:13:25 +0800

    css

---
+++ assets/js/custom-swiper-slider.js
@@ -0,0 +1,6 @@
+var swiper = new Swiper(".swiper-custom-slider", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
--- widgets/render.php
+++ widgets/render.php
@@ -1,27 +1,32 @@
-<div class="swiper-wrapper">
-    <?php foreach ($slides as $index => $slide) { ?>
-        <div class="swiper-slide">
-            <?php if (!empty($slide['image']['url'])) { ?>
-                <div class="bg-image">
-                    <img src="<?php echo esc_url($slide['image']['url']); ?>" alt="<?php echo esc_attr($slide['image']['alt']); ?>">
+<div class="swiper swiper-custom-slider">
+    <div class="swiper-wrapper">
+        <?php foreach ($slides as $index => $slide) { ?>
+            <div class="swiper-slide">
+                <div class="inner">
+                    <?php if (!empty($slide['image']['url'])) { ?>
+                        <div class="bg-image">
+                            <img src="<?php echo esc_url($slide['image']['url']); ?>" alt="<?php echo esc_attr($slide['image']['alt']); ?>">
+                        </div>
+                    <?php } ?>
+                    <?php if (!empty($slide['slide_heading'])) { ?>
+                        <div class="slide-heading">
+                            <h3><?php echo $slide['slide_heading']; ?></h3>
+                        </div>
+                    <?php } ?>
+                    <?php if (!empty($slide['slide_content'])) { ?>
+                        <div class="slide-content">
+                            <?php echo $slide['slide_content']; ?>
+                        </div>
+                    <?php } ?>
+                    <?php if (!empty($settings['button_text']) && !empty($settings['button_link']['url'])) { ?>
+                        <a class="slide-button" href="<?php echo esc_url($settings['button_link']['url']); ?>" <?php echo ($settings['button_link']['is_external']) ? 'target="_blank"' : ''; ?> <?php echo ($settings['button_link']['nofollow']) ? 'rel="nofollow"' : ''; ?>>
+                            <?php echo esc_html($settings['button_text']); ?>
+                        </a>
+                    <?php } ?>
                 </div>
-            <?php } ?>
-            <?php if (!empty($slide['slide_heading'])) { ?>
-                <div class="slide-heading">
-                    <h3><?php echo $slide['slide_heading']; ?></h3>
-                </div>
-            <?php } ?>
-            <?php if (!empty($slide['slide_content'])) { ?>
-                <div class="slide-content">
-                    <?php echo $slide['slide_content']; ?>
-                </div>
-            <?php } ?>
-            <?php if (!empty($settings['button_text']) && !empty($settings['button_link']['url'])) { ?>
-                <a class="slide-button" href="<?php echo esc_url($settings['button_link']['url']); ?>" <?php echo ($settings['button_link']['is_external']) ? 'target="_blank"' : ''; ?> <?php echo ($settings['button_link']['nofollow']) ? 'rel="nofollow"' : ''; ?>>
-                    <?php echo esc_html($settings['button_text']); ?>
-                </a>
-            <?php } ?>
-
-        </div>
-    <?php } ?>
+            </div>
+        <?php } ?>
+    </div>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
 </div>
