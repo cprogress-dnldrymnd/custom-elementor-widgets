f1703d4004982cae344a46a02c3728faeba9cf37
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-11 17:12:06 +0800

    Update render.php

--- widgets/render.php
+++ widgets/render.php
@@ -1,19 +1,27 @@
 <div class="swiper-wrapper">
-    <?php foreach ( $slides as $index => $slide ) : ?>
+    <?php foreach ($slides as $index => $slide) { ?>
         <div class="swiper-slide">
-            <?php if ( ! empty( $slide['image']['url'] ) ) : ?>
-                <img src="<?php echo esc_url( $slide['image']['url'] ); ?>" alt="<?php echo esc_attr( $slide['image']['alt'] ); ?>">
-            <?php endif; ?>
-            <div class="slide-content"><?php echo $slide['slide_content']; ?></div>
-
-            <?php if (!empty($settings['button_text']) && !empty($settings['button_link']['url'])): ?>
+            <?php if (!empty($slide['image']['url'])) { ?>
+                <div class="bg-image">
+                    <img src="<?php echo esc_url($slide['image']['url']); ?>" alt="<?php echo esc_attr($slide['image']['alt']); ?>">
+                </div>
+            <?php } ?>
+            <?php if (!empty($slide['slide_heading'])) { ?>
+                <div class="slide-heading">
+                    <h3><?php echo $slide['slide_heading']; ?></h3>
+                </div>
+            <?php } ?>
+            <?php if (!empty($slide['slide_content'])) { ?>
+                <div class="slide-content">
+                    <?php echo $slide['slide_content']; ?>
+                </div>
+            <?php } ?>
+            <?php if (!empty($settings['button_text']) && !empty($settings['button_link']['url'])) { ?>
                 <a class="slide-button" href="<?php echo esc_url($settings['button_link']['url']); ?>" <?php echo ($settings['button_link']['is_external']) ? 'target="_blank"' : ''; ?> <?php echo ($settings['button_link']['nofollow']) ? 'rel="nofollow"' : ''; ?>>
                     <?php echo esc_html($settings['button_text']); ?>
                 </a>
-            <?php endif; ?>
+            <?php } ?>
 
-            <?php // Example of using the index ?>
-            <span class="slide-number">Slide <?php echo $index + 1; ?></span>
         </div>
-    <?php endforeach; ?>
+    <?php } ?>
 </div>
