359371ae8ee342e345999a249a4bcada73e94eec
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-09-25 03:01:49 +0800

    css

--- widgets/step-slider/render.php
+++ widgets/step-slider/render.php
@@ -4,31 +4,35 @@ $args = array('post_type' => 'elementor_library', 'post__in' => $slide_ids);
 $loop = new WP_Query($args);
 ?>
 <section class="step-slider">
-    <div thumbsSlider class="swiper swiper-step-thumb">
-        <div class="swiper-wrapper">
-            <?php
-            foreach ($slide_ids as $slide_id) {
-                echo '<div class="swiper-slide">';
-                echo '<div class="inner">';
-                echo get_the_title($slide_id);
-                echo '</div>';
-                echo '</div>';
-            }
-            ?>
+    <div class="thumb-slider-holder">
+        <div thumbsSlider class="swiper swiper-step-thumb">
+            <div class="swiper-wrapper">
+                <?php
+                foreach ($slide_ids as $slide_id) {
+                    echo '<div class="swiper-slide">';
+                    echo '<div class="inner">';
+                    echo get_the_title($slide_id);
+                    echo '</div>';
+                    echo '</div>';
+                }
+                ?>
+            </div>
         </div>
     </div>
-    <div class="swiper swiper-step-slider">
-        <div class="swiper-wrapper">
-            <?php
-            foreach ($slide_ids as $slide_id) {
-                echo '<div class="swiper-slide">';
-                echo '<div class="inner">';
-                echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display($slide_id);
-                echo '</div>';
-                echo '</div>';
-            }
-            ?>
+    <div class="main-slider-holder">
+        <div class="swiper swiper-step-slider">
+            <div class="swiper-wrapper">
+                <?php
+                foreach ($slide_ids as $slide_id) {
+                    echo '<div class="swiper-slide">';
+                    echo '<div class="inner">';
+                    echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display($slide_id);
+                    echo '</div>';
+                    echo '</div>';
+                }
+                ?>
+            </div>
+            <div class="swiper-pagination"></div>
         </div>
-        <div class="swiper-pagination"></div>
     </div>
 </section>
