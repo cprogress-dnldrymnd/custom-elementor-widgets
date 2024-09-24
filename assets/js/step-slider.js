3a896b0a8037039644f773a2d4d76aacfd9189db
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-09-25 03:35:02 +0800

    Update step-slider.js

--- assets/js/step-slider.js
+++ assets/js/step-slider.js
@@ -1,7 +1,7 @@
 var swiper_step_thumb = new Swiper('.swiper-step-thumb', {
     loop: false,
     autoplay: false,
-    spaceBetween: 10,
+    spaceBetween: 40,
     navigation: {
         nextEl: ".swiper-button-next-step",
         prevEl: ".swiper-button-prev-step",
