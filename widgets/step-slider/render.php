0bda204334e38070b51ddea484142beb3f32cbd5
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-09-25 02:42:03 +0800

    Update render.php

--- widgets/step-slider/render.php
+++ widgets/step-slider/render.php
@@ -4,18 +4,31 @@ $args = array('post_type' => 'elementor_library', 'post__in' => $slide_ids);
 $loop = new WP_Query($args);
 ?>
 <section class="slider">
-  <div class="swiper swiper-is-vertical">
-    <div class="swiper-wrapper">
-      <?php
-      foreach ($slide_ids as $slide_id) {
-        echo '<div class="swiper-slide">';
-        echo '<div class="inner">';
-        echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display($slide_id);
-        echo '</div>';
-        echo '</div>';
-      }
-      ?>
+    <div class="swiper">
+        <div class="swiper-swapper">
+            <?php
+            foreach ($slide_ids as $slide_id) {
+                echo '<div class="swiper-slide">';
+                echo '<div class="inner">';
+                echo get_the_title($slide_id);
+                echo '</div>';
+                echo '</div>';
+            }
+            ?>
+        </div>
     </div>
-    <div class="swiper-pagination"></div>
-  </div>
+    <div class="swiper swiper-is-vertical">
+        <div class="swiper-wrapper">
+            <?php
+            foreach ($slide_ids as $slide_id) {
+                echo '<div class="swiper-slide">';
+                echo '<div class="inner">';
+                echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display($slide_id);
+                echo '</div>';
+                echo '</div>';
+            }
+            ?>
+        </div>
+        <div class="swiper-pagination"></div>
+    </div>
 </section>
