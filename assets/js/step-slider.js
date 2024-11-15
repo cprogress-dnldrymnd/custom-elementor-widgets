97347a465f6ce15973765ec5ed673fb1d4c4f783
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-15 11:59:36 +0800

    Update step-slider.js

--- assets/js/step-slider.js
+++ assets/js/step-slider.js
@@ -6,6 +6,7 @@ var swiper_step_thumb = new Swiper('.swiper-step-thumb', {
     spaceBetween: 20,
     slidesPerView: 'auto',
     watchSlidesProgress: true,
+    centeredSlides: true,
     navigation: {
         nextEl: ".swiper-button-next-step",
         prevEl: ".swiper-button-prev-step",
