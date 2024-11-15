277f4b8c9b679457b7db1050a9abc143765057a3
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-15 12:44:37 +0800

    Update step-slider.js

--- assets/js/step-slider.js
+++ assets/js/step-slider.js
@@ -18,13 +18,13 @@ var swiper_step_slider = new Swiper('.swiper-step-slider', {
             jQuery('.custom-pagination[key="' + index + '"]').addClass('active');
 
             if (index == 3) {
-                jQuery('.custom-pagination-holder').css('--offset', '-58px');
+                jQuery('.custom-pagination-holder').css('--offset', '-57x');
             } else if (index == 4) {
-                jQuery('.custom-pagination-holder').css('--offset', '-116px');
+                jQuery('.custom-pagination-holder').css('--offset', '-114px');
             } else if (index == 5) {
-                jQuery('.custom-pagination-holder').css('--offset', '-174px');
+                jQuery('.custom-pagination-holder').css('--offset', '-171px');
             } else if (index == 6) {
-                jQuery('.custom-pagination-holder').css('--offset', '-232px');
+                jQuery('.custom-pagination-holder').css('--offset', '-228px');
             } else {
                 jQuery('.custom-pagination-holder').css('--offset', '0');
             }
