a3dd5039ae5dfa93b21d8bfaf17d05c2356b521f
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-01-09 21:39:55 +0800

    Update step-slider.js

--- assets/js/step-slider.js
+++ assets/js/step-slider.js
@@ -29,7 +29,7 @@ var swiper_step_slider = new Swiper('.swiper-step-slider', {
 
             $height = jQuery('.swiper-step-slider .swiper-slide[key=' + index + ']').find('.inner').outerHeight();
 
-            jQuery('.swiper-step-slider .swiper-wrapper').height($height);
+            jQuery('.swiper-step-slider .swiper-wrapper').css('--height', $height + 'px');
         },
     }
 });
